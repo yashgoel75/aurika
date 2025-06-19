8187560a385a6a2bccf5dac60f9abfcbfd300238
yashgoel <yash.goel8370@gmail.com>
2025-06-19 15:41:27 +0530

    Header

--- app/Dashboard/page.js
+++ app/Dashboard/page.js
@@ -1,19 +1,25 @@
-'use client'
+"use client";
 import { useAccount } from "wagmi";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+
+import Header from "./Header/page";
+
 function Dashboard() {
-    const router = useRouter();
-    const account = useAccount();
-    useEffect(() => {
-        if (!account.isConnected) {
-            router.push('/');
-        }
-    }, [account.isConnected, router]);
+  const router = useRouter();
+  const account = useAccount();
+  useEffect(() => {
+    if (!account.isConnected) {
+      router.push("/");
+    }
+  }, [account.isConnected, router]);
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-3xl font-bold mb-4">Welcome to Aurika</h1>
-    </div>
+    <>
+      <Header />
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+        <h1 className="text-3xl font-bold mb-40">Welcome to Aurika</h1>
+      </div>
+    </>
   );
 }
 
