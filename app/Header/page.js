6f1fe479aebccb427e080b94751a72fc85e68193
yashgoel <yash.goel8370@gmail.com>
2025-06-20 23:46:17 +0530

    Responsive Navigation Bar

--- app/Header/page.js
+++ app/Header/page.js
@@ -4,8 +4,12 @@ import "./page.css";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import logo from "@/public/AurikaLogo.png";
+import { useState } from "react";
+import { set } from "mongoose";
 function Header() {
   const router = useRouter();
+  const [showNavigationBar, setShowNavigationBar] = useState(false);
+
   function handleDashboardNavigation() {
     router.push("/Dashboard");
   }
@@ -21,11 +25,114 @@ function Header() {
   function handleSettingsNavigation() {
     router.push("/Settings");
   }
+  function handleNavigationBar() {
+    setShowNavigationBar(!showNavigationBar);
+  }
   return (
     <>
+      {showNavigationBar ? (
+        <div className="navigationOnMobile">
+          <div className="flex flex-col w-10/10 justify-center p-4 bg-slate-100 shadow-lg rounded-lg text-lg text-stone-800">
+            <div className="flex justify-between items-center hover:cursor-pointer">
+              <Image src={logo} alt="Aurika Logo" width={200} onClick={handleDashboardNavigation}/>
+              <svg
+                onClick={handleNavigationBar}
+                xmlns="http://www.w3.org/2000/svg"
+                height="35px"
+                viewBox="0 -960 960 960"
+                width="35px"
+                fill="#000000"
+              >
+                <path d="m336-280-56-56 144-144-144-143 56-56 144 144 143-144 56 56-144 143 144 144-56 56-143-144-144 144Z" />
+              </svg>
+            </div>
+            <div>
+              <ul className="flex flex-col pl-6 space-y-4 text-lg text-stone-800 mt-4 mb-4">
+                <li
+                  className="flex flex-row items-center hover:cursor-pointer"
+                  onClick={handleDashboardNavigation}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="24px"
+                    viewBox="0 -960 960 960"
+                    width="24px"
+                    fill="#000000"
+                  >
+                    <path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z" />
+                  </svg>
+                  &nbsp;Dashboard
+                </li>
+                <li
+                  className="flex flex-row items-center hover:cursor-pointer"
+                  onClick={handlePortfolioNavigation}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="24px"
+                    viewBox="0 -960 960 960"
+                    width="24px"
+                    fill="#000000"
+                  >
+                    <path d="M200-280v-280h80v280h-80Zm240 0v-280h80v280h-80ZM80-120v-80h800v80H80Zm600-160v-280h80v280h-80ZM80-640v-80l400-200 400 200v80H80Zm178-80h444-444Zm0 0h444L480-830 258-720Z" />
+                  </svg>
+                  &nbsp;Portfolio
+                </li>
+                <li
+                  className="flex flex-row items-center hover:cursor-pointer"
+                  onClick={handleOrdersNavigation}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="24px"
+                    viewBox="0 -960 960 960"
+                    width="24px"
+                    fill="#000000"
+                  >
+                    <path d="M120-80v-800l60 60 60-60 60 60 60-60 60 60 60-60 60 60 60-60 60 60 60-60 60 60 60-60v800l-60-60-60 60-60-60-60 60-60-60-60 60-60-60-60 60-60-60-60 60-60-60-60 60Zm120-200h480v-80H240v80Zm0-160h480v-80H240v80Zm0-160h480v-80H240v80Zm-40 404h560v-568H200v568Zm0-568v568-568Z" />
+                  </svg>
+                  &nbsp;Orders
+                </li>
+                <li
+                  className="flex flex-row items-center hover:cursor-pointer"
+                  onClick={handleAccountNavigation}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="24px"
+                    viewBox="0 -960 960 960"
+                    width="24px"
+                    fill="#000000"
+                  >
+                    <path d="M234-276q51-39 114-61.5T480-360q69 0 132 22.5T726-276q35-41 54.5-93T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 59 19.5 111t54.5 93Zm246-164q-59 0-99.5-40.5T340-580q0-59 40.5-99.5T480-720q59 0 99.5 40.5T620-580q0 59-40.5 99.5T480-440Zm0 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q53 0 100-15.5t86-44.5q-39-29-86-44.5T480-280q-53 0-100 15.5T294-220q39 29 86 44.5T480-160Zm0-360q26 0 43-17t17-43q0-26-17-43t-43-17q-26 0-43 17t-17 43q0 26 17 43t43 17Zm0-60Zm0 360Z" />
+                  </svg>
+                  &nbsp;Account
+                </li>
+                <li
+                  className="flex flex-row items-center hover:cursor-pointer"
+                  onClick={handleSettingsNavigation}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="24px"
+                    viewBox="0 -960 960 960"
+                    width="24px"
+                    fill="#000000"
+                  >
+                    <path d="m370-80-16-128q-13-5-24.5-12T307-235l-119 50L78-375l103-78q-1-7-1-13.5v-27q0-6.5 1-13.5L78-585l110-190 119 50q11-8 23-15t24-12l16-128h220l16 128q13 5 24.5 12t22.5 15l119-50 110 190-103 78q1 7 1 13.5v27q0 6.5-2 13.5l103 78-110 190-118-50q-11 8-23 15t-24 12L590-80H370Zm70-80h79l14-106q31-8 57.5-23.5T639-327l99 41 39-68-86-65q5-14 7-29.5t2-31.5q0-16-2-31.5t-7-29.5l86-65-39-68-99 42q-22-23-48.5-38.5T533-694l-13-106h-79l-14 106q-31 8-57.5 23.5T321-633l-99-41-39 68 86 64q-5 15-7 30t-2 32q0 16 2 31t7 30l-86 65 39 68 99-42q22 23 48.5 38.5T427-266l13 106Zm42-180q58 0 99-41t41-99q0-58-41-99t-99-41q-59 0-99.5 41T342-480q0 58 40.5 99t99.5 41Zm-2-140Z" />
+                  </svg>
+                  &nbsp;Settings
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      ) : null}
+
       <div className="flex items-center justify-between p-4 bg-gray-100 text-lg text-white">
-        <Image src={logo} alt="Aurika Logo" width={200} />
+        <Image src={logo} alt="Aurika Logo" width={200} onClick={handleDashboardNavigation}/>
         <svg
+          onClick={handleNavigationBar}
           xmlns="http://www.w3.org/2000/svg"
           height="24px"
           viewBox="0 -960 960 960"
